35c559fe77b0c60c30dcbfe018727451eefb3f74
engineer2u4u <48150735+engineer2u4u@users.noreply.github.com>
2019-04-11 05:03:25 +0530

    Final commit

--- server.js
+++ server.js
@@ -1,8 +1,56 @@
 var express = require('express');
-var clients=[];
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), () => console.log('Example app listening on port '+app.get('port')+' !'));
-//app.use(express.static('public'));
 console.log("My server is running");
+app.get('/', (req, res) => res.send('Working Fine'));
+var clients={};
+var socket =  require('socket.io');
+var io = socket(server);
+io.sockets.on('connection', newConnection);
 
+function newConnection(socket)
+{
+	var current ;
+	console.log("new connection " + socket.id);
+  socket.on('twitchSocket',function(twitchId){
+		if(!(twitchId in clients))
+		{
+		  socket.twitchId = twitchId;
+		  clients[socket.twitchId] = socket;
+			console.log("Connected Users"+Object.keys(clients).toString());
+		 }
+	  else
+	  {
+	  console.log("Current Users"+Object.keys(clients).toString());
+	  }
+	});
+	socket.on('imageStream',function(twitchId,imageData){
+		if(twitchId in clients)
+		 {
+			 console.log("sending to "+twitchId);
+			 clients[twitchId].emit('imageStream',imageData);
+		 }
+		else
+		{
+			console.log("not sending to"+twitchId);
+		}
+	});
+	
+	socket.on('disconnect',function(data){
+		console.log("trying to disconnect");
+	if(!socket.twitchId)
+	{
+		return;
+	}
+		else
+		{
+		console.log("closed"+socket.twitchId);
+		delete clients[socket.twitchId];
+		}
+		
+	});
+  
+  
+}
